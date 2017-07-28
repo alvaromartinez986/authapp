a8e2d7acd6cdbc4411ea45428a8c1384c6c2262f
Alvaro Martinez <msae986@gmail.com>
2017-07-28 10:34:49 -0500

    First commmit with the home page and auth0 system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { APP_ROUTING } from './app.routes';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { ProtectedComponent } from './components/protected/protected.component';
+
+//services
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PricesComponent,
+    ProtectedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	APP_ROUTING
   ],
-  providers: [],
+  providers: [
+	  AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
